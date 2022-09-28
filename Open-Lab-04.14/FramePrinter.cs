105b6a14d5553737de4bc45a818d9e78132d2b1f
Pho3nixG0D <oliverkrskagmail.com>
2022-09-28 16:04:09 +0200

    checking string with max length, create width of strings, "*" line, writing lines with strings, last "*" line

--- Open-Lab-04.14/FramePrinter.cs
+++ Open-Lab-04.14/FramePrinter.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Linq;
+using System.Xml.Linq;
 
 namespace Open_Lab_04._14
 {
     public class FramePrinter
     {
         public void Print(string[] strings)
-        {
-            throw new NotImplementedException();
+        {   
+            //checking strings length
+            int wordLength = 0;
+            for (int i = 0; i < strings.Length; i++)
+            {
+                if (wordLength < strings[i].Length)
+                {
+                    wordLength = strings[i].Length;
+                }
+            }
+            // creating width of lines
+            int width = 4 + wordLength;
+            // firs row of "*"
+            Console.WriteLine(String.Concat(Enumerable.Repeat("*", width)));
+            //writing lines with words
+            for (int i = 0; i < strings.Length; i++)
+            {
+                Console.Write("* ");
+                Console.Write(strings[i]);
+                for (int j = 0; j < width - strings[i].Length - 4; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine(" *");
+            }
+            //last line "*"
+            Console.WriteLine(String.Concat(Enumerable.Repeat("*", width)));
         }
     }
 }
